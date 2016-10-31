1b1d9cac3b79ba37f6adf6e073fd22ecde20304f
sv ramu <svramu@gmail.com>
2016-10-31 12:16:08 +0530

    3-ethyl-2,7-dimethyloctane working.

--- iupac.ts
+++ iupac.ts
@@ -1,4 +1,17 @@
 namespace iupac {
+  let noname = [
+    '',
+    '',
+    'di',
+    'tri',
+    'tetra',
+    'penta',
+    'hexa',
+    'hepta',
+    'octa',
+    'nona',
+    'deca',
+  ]
   let cname = [
     '',
     'meth',
@@ -143,22 +156,52 @@ namespace iupac {
       if (todo) this.build()
     }
     private subCore(s: string): string {
+      if (s === '' || s === undefined || s === null) return ''
       return s.substr(s.indexOf('-') + 1)
+    }
+    private subCorePrefix(s: string): string {
+      if (s === '' || s === undefined || s === null) return ''
+      return s.substring(s.indexOf('-'), 0)
     }
     private compare(a, b): number {
       return (a > b) ? 1 : (a === b) ? 0 : -1
     }
-    private sortSubstituenets(subs: string[]): string[] {
-      subs = subs.sort((a, b) => {
-        let aa = this.subCore(a)
-        let bb = this.subCore(b)
-        return this.compare(aa, bb)
-      })
-      return subs
+    private coreCompare(a, b): number {
+      let aa = this.subCore(a)
+      let bb = this.subCore(b)
+      return this.compare(aa, bb)
+    }
+    private coreEquals(a, b): boolean {
+      return this.coreCompare(a, b) === 0
+    }
+    private unify(sames: string[]): string {
+      if (sames === null || sames.length < 1) return ''
+      if (sames.length === 1) return sames[0]
+      let prefix = noname[sames.length]
+      let suffix = this.subCore(sames[0])
+      let locants = []
+      sames.forEach(s => locants.push(this.subCorePrefix(s)))
+      let locant = locants.join(',')
+      console.log('prefix: ' + prefix + ' suffix: ' + suffix + ' locant: ' + locant);
+      return locant + '-' + prefix + suffix
     }
     private normalizeSubstituenets(subs: string[]): string[] {
-      subs = this.sortSubstituenets(subs)
-      return subs
+      subs = subs.sort((a, b) => this.coreCompare(a, b))
+      console.log('[] ' + subs);
+      let newSubs = []
+      let sames = []
+      let prev = null
+      subs.forEach(s => {
+        let eq = this.coreEquals(s, prev)
+        if ((sames.length > 0) && !eq) {
+          newSubs.push(this.unify(sames))
+          sames = []
+        }
+        prev = s
+        sames.push(s)
+      })
+      newSubs.push(this.unify(sames))
+      return newSubs
     }
     public iupac(): string {
       let branches = {}
